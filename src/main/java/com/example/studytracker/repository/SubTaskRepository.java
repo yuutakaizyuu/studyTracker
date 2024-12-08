1b7f469e9e9485a9c7f07dae2f741f2421e145b9
masyuuuu31 <masyuuuu31@outlook.jp>
2024-12-08 15:42:32 +0900

    エラー修正 @ritsu.inoue

--- src/main/java/com/example/studytracker/repository/SubTaskRepository.java
+++ src/main/java/com/example/studytracker/repository/SubTaskRepository.java
@@ -1,8 +1,9 @@
 package com.example.studytracker.repository;
 
+import com.example.studytracker.entity.SubTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubTaskRepository extends JpaRepository<SubTaskRepository, Long> {
+public interface SubTaskRepository extends JpaRepository<SubTask, Long> {
 }
